dc8eb2d01c7c0e0ca663393ee00da67a8c084e6b
Aaron Bolton <aaron@boltonstudios.com>
2023-12-24 12:17:09 -0500

    Removed relative import for models

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from datetime import datetime
 import requests
 from dotenv import load_dotenv
 import psycopg2
+from dataclasses import dataclass
 
 # Flask imports
 from flask import Flask, Response, redirect, render_template, request, url_for
@@ -103,7 +104,58 @@ with app.app_context():
     init_db()
 
 # Import models.
-from .models import User, Extension
+#from .models import User, Extension
+
+# Define the user table class.
+@dataclass
+class User( db.Model ):
+
+    # pylint: disable=too-many-instance-attributes
+    # Eight is reasonable in this case.
+
+    """
+    Class to define the User table.
+    """
+    instance_id: db.Column      = db.Column( db.String( 200 ), primary_key = True, unique = True )
+    site_id: db.Column          = db.Column( db.String( 200 ), unique = True )
+    extensions                  = db.relationship( 'Extension', backref = 'user' )
+    refresh_token: db.Column    = db.Column( db.String( 200 ) ) # Not unique because...?
+    is_free: db.Column          = db.Column( db.Boolean )
+    created_at: db.Column       = db.Column( db.DateTime( timezone = True ),
+                                        server_default = func.now() )
+
+    def __repr__( self ):
+        return f'<user { self.instance_id }>'
+
+# Define the slider extension table class.
+@dataclass
+class Extension( db.Model ):
+
+    # pylint: disable=too-many-instance-attributes
+    # Eight is reasonable in this case.
+
+    """
+    Class to define the Slider extension table.
+    """
+    extension_id: db.Column                 = db.Column( db.String( 80 ), primary_key = True, unique = True )
+    instance_id: db.Column                  = db.Column( db.String( 80 ), db.ForeignKey( User.instance_id ) )
+    before_image: db.Column                 = db.Column( db.String( 1000 ) )
+    before_label_text: db.Column            = db.Column( db.String( 1000 ) )
+    before_alt_text: db.Column              = db.Column( db.String( 1000 ) )
+    after_image: db.Column                  = db.Column( db.String( 1000 ) )
+    after_label_text: db.Column             = db.Column( db.String( 1000 ) )
+    after_alt_text: db.Column               = db.Column( db.String( 1000 ) )
+    offset: db.Column                       = db.Column( db.Integer )
+    offset_float: db.Column                 = db.Column( db.Float )
+    is_vertical: db.Column                  = db.Column( db.Boolean )
+    mouseover_action: db.Column             = db.Column( db.Integer, default = 1 )
+    handle_animation: db.Column             = db.Column( db.Integer, default = 0 )
+    is_move_on_click_enabled: db.Column     = db.Column( db.Boolean )
+    created_at: db.Column                   = db.Column( db.DateTime( timezone = True ),
+                                                server_default = func.now() )
+
+    def __repr__( self ):
+        return f'<slider { self.extension_id } in { self.instance_id }>'
 
 # Wix Constants
 WEBHOOK_PUBLIC_KEY = os.getenv("WEBHOOK_PUBLIC_KEY")
